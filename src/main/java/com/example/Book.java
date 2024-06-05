566188b1f2074f17983224b4d19ad6ebed76a0f9
rashtrapal143 <rashtraapalw143@gmail.com>
2024-06-06 02:11:27 +0530

    all done

--- src/main/java/com/example/Book.java
+++ src/main/java/com/example/Book.java
@@ -1,6 +1,8 @@
+package com.example;
 /*
  * Requirements
 Class Definition:
+
 
 Define a class named Book.
 Instance Variables:
@@ -25,3 +27,59 @@ think about edge cases, such as setting negative values for the number of copies
 After implementing the class, test it with the example usage provided to ensure everything works correctly.
 * 
  */
+
+ public class Book {
+    // Instance variables
+    private String title;
+    private String author;
+    private int numberOfCopies;
+    private static int totalBooks = 0; 
+
+    // Constructor
+    public Book(String title, String author, int numberOfCopies) {
+        this.title = title;
+        this.author = author;
+        this.numberOfCopies = numberOfCopies;
+        totalBooks++; 
+
+    // Getters and setters
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getNumberOfCopies() {
+        return numberOfCopies;
+    }
+
+    public void setNumberOfCopies(int numberOfCopies) {
+       
+            this.numberOfCopies = numberOfCopies;
+     
+    }
+
+    // Additional methods
+    public void displayBookDetails() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Number of Copies: " + numberOfCopies);
+    }
+
+   
+}
+ }
+
+   
+
+       
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -8,21 +8,30 @@ public class Main {
         // Create Student objects
         Student student1 = new Student("Alice", 20);
         Student student2 = new Student("Bob", 22);
+        Book book1 = new Book("Harry Potter", "Rowling", 100);
+        
     
         // Use getters
         System.out.println("Student 1 Name: " + student1.getName());
         System.out.println("Student 1 Age: " + student1.getAge());
+        System.out.println("Updated Number of Copies: " + book1.getNumberOfCopies());
     
         // Use setters
         student1.setName("Alicia");
         student1.setAge(21);
+        book1.setNumberOfCopies(90);
     
         // Display student details
         student1.displayDetails();
         student2.displayDetails();
+        book1.displayBookDetails();
+        System.out.println("Updated Number of Copies: " + book1.getNumberOfCopies());
     
         // Use static function
-        System.out.println("Total Students: " + Student.getStudentCount());
+        //System.out.println("Total Students: " + Student.getStudentCount());
+
+        
+
     }
     
 
